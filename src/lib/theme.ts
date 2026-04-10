ea7594d70960a2b53339777d5667b59f8391611b
adsuth <adamsutherlandam@gmail.com>
2026-04-10 23:40:13 +0100

    fix theme.ts for deploy

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,12 +1,10 @@
 "use client";
 import { createTheme } from "@mui/material/styles";
-import "@/style.css"
 
 const theme = createTheme({
   typography: {
     fontFamily: "var(--font-roboto)",
   },
-  cssVariables: true,
 });
 
 export default theme;
